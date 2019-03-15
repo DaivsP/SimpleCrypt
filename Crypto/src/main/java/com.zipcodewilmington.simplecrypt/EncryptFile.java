40b68b62c857a9149a217f0f7e61978c7b339459
Davis <davisperchik@gmail.com>
2019-03-15 11:10:29 -0400

    now reading text from files

--- Crypto/src/main/java/com.zipcodewilmington.simplecrypt/EncryptFile.java
+++ Crypto/src/main/java/com.zipcodewilmington.simplecrypt/EncryptFile.java
@@ -14,8 +14,12 @@ public class EncryptFile extends ROT13 {
     }
 
     public String encryptFile() throws IOException {
-        Stream<String> bands = (Stream<String>) Files.readAllLines(Paths.get("sonnet18.txt"));
-        List<String> answer =bands.map(this::encrypt).collect(Collectors.toList());
+        String currentProjectDirectory = System.getProperty("user.dir");
+        String resourceDirectory = currentProjectDirectory + "/Crypto/src/main/resources";
+        String fileName = resourceDirectory + "/sonnet18.txt";
+        System.out.println(currentProjectDirectory);
+        Stream<String> bands = Files.readAllLines(Paths.get(fileName)).stream();
+        List<String> answer = bands.map(this::encrypt).collect(Collectors.toList());
         bands.close();
         String returnString = "";
         for (String string : answer){
