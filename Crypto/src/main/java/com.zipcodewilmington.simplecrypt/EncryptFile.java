312d0dc0aed4d4dee75a18afabf7b52dc6400785
Davis <davisperchik@gmail.com>
2019-03-15 11:32:35 -0400

    cleaned up encryptFile code

--- Crypto/src/main/java/com.zipcodewilmington.simplecrypt/EncryptFile.java
+++ Crypto/src/main/java/com.zipcodewilmington.simplecrypt/EncryptFile.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,16 +15,35 @@ public class EncryptFile extends ROT13 {
     }
 
     public String encryptFile() throws IOException {
+        Function<String, String > function = s -> encrypt(s);
+        return action(function);
+    }
+
+    public String decryptFile() throws IOException{
+        Function<String, String > function = s -> decrypt(s);
+        return action(function);
+    }
+
+    private Stream<String> getFileToStream() throws IOException {
         String currentProjectDirectory = System.getProperty("user.dir");
         String resourceDirectory = currentProjectDirectory + "/Crypto/src/main/resources";
         String fileName = resourceDirectory + "/sonnet18.txt";
-        System.out.println(currentProjectDirectory);
-        Stream<String> bands = Files.readAllLines(Paths.get(fileName)).stream();
-        List<String> answer = bands.map(this::encrypt).collect(Collectors.toList());
+        //System.out.println(currentProjectDirectory);
+        return Files.readAllLines(Paths.get(fileName)).stream();
+    }
+
+    private String action(Function function) throws IOException {
+        Stream<String> bands = getFileToStream();
+        List<String> answer = (List<String>) bands.map(function).collect(Collectors.toList());
         bands.close();
+        String returnString = getString(answer);
+        return returnString;
+    }
+
+    private String getString(List<String> answer) {
         String returnString = "";
         for (String string : answer){
-            returnString += string;
+            returnString += string + " \n";
         }
         return returnString;
     }
--- Crypto/src/test/java/com.zipcodewilmington.simplecrypt/EncryptFileTest.java
+++ Crypto/src/test/java/com.zipcodewilmington.simplecrypt/EncryptFileTest.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.simplecrypt;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,6 +12,27 @@ public class EncryptFileTest {
 
     @Test
     public void encryptFile() throws IOException {
-        System.out.println(encryptFile.encryptFile());
+        String expected = "Funyy V pbzcner gurr gb n fhzzre’f qnl? \n" +
+                "Gubh neg zber ybiryl naq zber grzcrengr: \n" +
+                "Ebhtu jvaqf qb funxr gur qneyvat ohqf bs Znl, \n" +
+                "Naq fhzzre’f yrnfr ungu nyy gbb fubeg n qngr; \n" +
+                "Fbzrgvzr gbb ubg gur rlr bs urnira fuvarf, \n" +
+                "Naq bsgra vf uvf tbyq pbzcyrkvba qvzz'q; \n" +
+                "Naq rirel snve sebz snve fbzrgvzr qrpyvarf, \n" +
+                "Ol punapr be angher’f punatvat pbhefr hagevzz'q; \n" +
+                "Ohg gul rgreany fhzzre funyy abg snqr, \n" +
+                "Abe ybfr cbffrffvba bs gung snve gubh bj’fg; \n" +
+                "Abe funyy qrngu oent gubh jnaqre’fg va uvf funqr, \n" +
+                "Jura va rgreany yvarf gb gvzr gubh tebj’fg: \n" +
+                "   Fb ybat nf zra pna oerngur be rlrf pna frr, \n" +
+                "   Fb ybat yvirf guvf, naq guvf tvirf yvsr gb gurr. \n";
+        String actual = encryptFile.encryptFile();
+        Assert.assertEquals(expected, actual);
+        //System.out.println(encryptFile.encryptFile());
+    }
+
+    @Test
+    public void decryptFile(){
+
     }
 }
